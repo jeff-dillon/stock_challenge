532ef7ad632576332399693742708ccd1c686e70
jeff-dillon <dillonjg@gmail.com>
2022-05-10 16:36:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Stock Challenge
+
+## Requirements
+
+You are given a csv file (stocks.csv) containing stock symbols, their current price, as well as the price change of the previous day.
+
+As you plan to take some profits, write a program that creates a list of all the stock symbols with a change of more than +5 percent. This list should only contain the stock symbol, not the price or the absolute change.
+
+Challenge: Implement the requierments using Pandas.
+
+Bonus: Implement the requirements withouut using Pandas.
+
+
+## Instructions
+
+1. install the required packages `python -m pip install -r requirements.txt`
+1. add your code to the challenge.py file
---
+++ challenge.py
@@ -0,0 +1,23 @@
+# Instructions
+# You are given a csv file (stocks.csv) containing stock symbols, their current 
+# price, as well as the price change of the previous day.
+# 
+# As you plan to take some profits, write a program that creates a list of all 
+# the stock symbols with a change of more than +5 percent. This list should only 
+# contain the stock symbol, not the price or the absolute change.
+# 
+# Challenge 1: Implement the requierments using Pandas.
+# Challenge 2: Implement the requirements withouut using Pandas.
+
+import pandas as pd
+
+#
+# Challenge 1: Implement Pandas solution here
+# 
+
+
+import csv
+
+#
+# Challenge 2: Implement non-Pandas solution here
+# Hint: 
